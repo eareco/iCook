8ed55534aa5a63b9c589d799174e8b34e5df1a6b
eareco <eareco@udesa.edu.ar>
2026-06-01 20:24:59 -0300

    primer push

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFu0IeX34ndeNIFfuMrH1mn-qCHVX8DPU",
+  authDomain: "integradorarecolaurenz.firebaseapp.com",
+  projectId: "integradorarecolaurenz",
+  storageBucket: "integradorarecolaurenz.firebasestorage.app",
+  messagingSenderId: "551390235443",
+  appId: "1:551390235443:web:a899423129270906568c46"
+};
+
+app.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const db = app.firebase();
+
