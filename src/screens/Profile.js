a5ec41eba2fcb97f0b2a5c150b3c7c784bf2b44e
eareco <eareco@udesa.edu.ar>
2026-06-09 19:22:26 -0300

    Hice un cambio en el perfil para que aparezca el nombre de usuario, el mail y los ultimos posteos

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,43 +1,150 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { auth, db } from "../firebase/config";
 
 function Profile(props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Mi Perfil</Text>
+    const [misPosts, setMisPosts] = useState([]);
+    const [username, setUsername] = useState("");
 
-      <Pressable
-        style={styles.boton}
-        onPress={() => props.navigation.navigate("Login")}
-      >
-        <Text style={styles.textoBoton}>Desloguearse</Text>
-      </Pressable>
-    </View>
-  );
+    const email = auth.currentUser.email;
+
+    useEffect(() => {
+        db.collection("users")
+            .where("email", "==", email)
+            .onSnapshot((docs) => {
+                docs.forEach((doc) => {
+                    setUsername(doc.data().username);
+                });
+            });
+
+        db.collection("posts")
+            .where("email", "==", email)
+            .orderBy("createdAt", "desc")
+            .onSnapshot((docs) => {
+                let posts = [];
+
+                docs.forEach((doc) => {
+                    posts.push({
+                        id: doc.id,
+                        data: doc.data()
+                    });
+                });
+
+                setMisPosts(posts);
+            });
+    }, []);
+
+    function logout() {
+        auth.signOut()
+            .then(() => {
+                props.navigation.navigate("Login");
+            })
+            .catch(error => console.log(error));
+    }
+
+    return (
+        <View style={styles.container}>
+
+            <Text style={styles.nombre}>{username}</Text>
+
+            <Text style={styles.email}>
+                {email}
+            </Text>
+
+            <Text style={styles.subtitulo}>
+                Últimos posteos
+            </Text>
+
+            <FlatList
+                data={misPosts}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <View style={styles.post}>
+
+                        <Text style={styles.fecha}>
+                            Posteo: {new Date(item.data.createdAt).toLocaleString()}
+                        </Text>
+
+                        <Text style={styles.descripcion}>
+                            {item.data.descripcionPost}
+                        </Text>
+
+                    </View>
+                )}
+            />
+
+            <Pressable
+                style={styles.botonCerrar}
+                onPress={logout}
+            >
+                <Text style={styles.textoCerrar}>
+                    Cerrar sesión
+                </Text>
+            </Pressable>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    justifyContent: "center",
-    padding: 20,
-  },
-  titulo: {
-    fontSize: 28,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 30,
-  },
-  boton: {
-    backgroundColor: "#a63e4d",
-    padding: 14,
-    borderRadius: 8,
-    alignItems: "center",
-  },
-  textoBoton: {
-    color: "white",
-    fontSize: 18,
-  },
+
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        padding: 20,
+    },
+
+    nombre: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginTop: 20,
+    },
+
+    email: {
+        color: "gray",
+        fontSize: 16,
+        marginBottom: 25,
+    },
+
+    subtitulo: {
+        fontSize: 26,
+        fontWeight: "bold",
+        marginBottom: 15,
+    },
+
+    post: {
+        borderWidth: 2,
+        borderColor: "#a63e4d",
+        borderRadius: 15,
+        padding: 15,
+        marginBottom: 15,
+        backgroundColor: "#ebc9c6",
+    },
+
+    fecha: {
+        color: "gray",
+        fontSize: 12,
+        marginBottom: 8,
+    },
+
+    descripcion: {
+        fontSize: 18,
+    },
+
+    botonCerrar: {
+        backgroundColor: "#f4b6b6",
+        padding: 15,
+        borderRadius: 12,
+        alignItems: "center",
+        marginTop: 10,
+        marginBottom: 20,
+    },
+
+    textoCerrar: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+
 });
 
 export default Profile;
